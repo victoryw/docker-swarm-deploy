004a15dbfeceeaa628ea27f51a47b8d38ef13cf3
tianyawy <tianya_wy@hotmail.com>
2019-05-16 15:24:28 +0800

    [victoryw] init

--- src/main/kotlin/com/example/demo/DemoApplication.kt
+++ src/main/kotlin/com/example/demo/DemoApplication.kt
@@ -1,11 +1,28 @@
 package com.example.demo
 
+import com.sun.org.apache.bcel.internal.generic.GETFIELD
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.reactive.function.BodyInserters.fromObject
+import org.springframework.web.reactive.function.server.RouterFunction
+import org.springframework.web.reactive.function.server.ServerResponse
+import org.springframework.web.reactive.function.server.router
 
 @SpringBootApplication
 class DemoApplication
 
 fun main(args: Array<String>) {
 	runApplication<DemoApplication>(*args)
+}
+
+@Configuration
+class SimpleRoute {
+	@Bean
+	fun route(): RouterFunction<ServerResponse> {
+		return router {
+			GET("/route") { _ -> ServerResponse.ok().body(fromObject(arrayOf(1, 2, 3))) }
+		}
+	}
 }
