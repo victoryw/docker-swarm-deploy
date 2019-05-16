77f6fd311d5100a340a55abd1891c9ac1bb86660
tianyawy <tianya_wy@hotmail.com>
2019-05-16 20:45:17 +0800

    [victoryw] init

--- src/main/kotlin/com/example/demo/DemoApplication.kt
+++ src/main/kotlin/com/example/demo/DemoApplication.kt
@@ -1,14 +1,16 @@
 package com.example.demo
 
-import com.sun.org.apache.bcel.internal.generic.GETFIELD
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.http.HttpStatus
 import org.springframework.web.reactive.function.BodyInserters.fromObject
 import org.springframework.web.reactive.function.server.RouterFunction
 import org.springframework.web.reactive.function.server.ServerResponse
 import org.springframework.web.reactive.function.server.router
+import reactor.core.publisher.Mono
+import java.io.File
 
 @SpringBootApplication
 class DemoApplication
@@ -22,7 +24,15 @@ class SimpleRoute {
 	@Bean
 	fun route(): RouterFunction<ServerResponse> {
 		return router {
-			GET("/route") { _ -> ServerResponse.ok().body(fromObject(arrayOf(1, 2, 3))) }
+			GET("/route") { _ -> mono() }
 		}
+	}
+
+	private fun mono(): Mono<ServerResponse> {
+		if(File("/var/log/1.log").exists()) {
+			return ServerResponse.status(HttpStatus.INTERNAL_SERVER_ERROR).render("")
+		}
+
+		return ServerResponse.ok().body(fromObject(arrayOf(1, 2, 3, 4)))
 	}
 }
